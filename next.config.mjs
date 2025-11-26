0b0f776f259cf9c10aa64471b51feecb5f23c0de
shruti0101 <shrutiguptacu@gmail.com>
2025-11-26 13:23:43 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,34 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//      reactStrictMode: true,
+//   images: {
+//     domains: ['cdn.sanity.io'],
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     reactStrictMode: true,
+  reactStrictMode: true,
+
   images: {
-    domains: ['cdn.sanity.io'],
+    unoptimized: true, // Important for Next 15 static images
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+    localPatterns: [
+      {
+        pathname: "/**", // allow ALL public images
+        search: "",
+      },
+    ],
   },
 };
 
