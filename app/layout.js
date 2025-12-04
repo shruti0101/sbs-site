cbd447381eae9edcd23f932202e65a65fd520b31
shruti0101 <shrutiguptacu@gmail.com>
2025-12-04 17:02:56 +0530

    adding gtm code

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,9 @@
-
-import { Roboto ,Poppins} from "next/font/google"; 
+import { Roboto, Poppins } from "next/font/google";
 import "./globals.css";
 import LayoutWrapper from "@/components/LayoutWrapper";
-
 import Social from "@/components/Landingpage/Social";
 import Stickybutton from "@/components/Landingpage/Stickybutton";
-
+import Script from "next/script";
 
 // Roboto
 const roboto = Roboto({
@@ -16,31 +14,51 @@ const roboto = Roboto({
 
 const poppins = Poppins({
   variable: "--font-poppins",
-  subsets:["latin"],
-  weight:["300", "400", "500", "700"]
-})
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+});
 
 export const metadata = {
   title: "SBS Machinery – Paper Cup Making Machines Manufacturer in Delhi India",
-  description: "Discover top-quality paper cup Making machines manufactured in Delhi, India. Enhance your production efficiency with our innovative and reliable solutions.",
+  description:
+    "Discover top-quality paper cup Making machines manufactured in Delhi, India. Enhance your production efficiency with our innovative and reliable solutions.",
   icons: {
-    icon: "/logo.webp", 
-  
+    icon: "/logo.webp",
   },
 };
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`  ${roboto.variable} ${poppins.variable} antialiased`}
-      >
-        
-          <LayoutWrapper>
-             <Social></Social>
-   <Stickybutton></Stickybutton>
-            {children}</LayoutWrapper>
+      {/* Google Tag Manager */}
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KVG53JFL');
+        `}
+      </Script>
+      {/* End Google Tag Manager */}
+
+      <body className={`${roboto.variable} ${poppins.variable} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KVG53JFL"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        <LayoutWrapper>
+          <Social />
+          <Stickybutton />
+          {children}
+        </LayoutWrapper>
       </body>
     </html>
   );
